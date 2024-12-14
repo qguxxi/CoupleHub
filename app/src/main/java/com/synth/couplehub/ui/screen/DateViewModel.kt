132af617a19dfff4a0595908b8c2dd3dd8eb1c25
Duc Hoang Manh <jandyz.dev@gmail.com>
2024-12-15 00:33:48 +0700

    adjusted bottom navigation bar

--- app/src/main/java/com/synth/couplehub/ui/screen/DateViewModel.kt
+++ app/src/main/java/com/synth/couplehub/ui/screen/DateViewModel.kt
@@ -1,8 +1,6 @@
 package com.synth.couplehub.ui.screen
 
-import android.content.Context
 import androidx.lifecycle.ViewModel
-import com.synth.couplehub.data.local.UserPreference
 
 class DateViewModel : ViewModel() {
 
